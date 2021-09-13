cedb3951e743ba2e15b577977580b0b196501e08
Max Kayander <maxim.kayander1@gmail.com>
2021-09-13 16:25:24 +0300

    Use app styles as css module.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import "./App.css";
 import apollo from "./api/apollo";
 import { LaunchesList } from "./components";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import { GetLaunchesDocument, GetLaunchesQuery, GetLaunchesQueryVariables } from "./api/generated/queries";
 import { Container } from "react-bootstrap";
+import styles from "./App.module.scss";
 
 function App() {
   useEffect(() => {
@@ -18,7 +18,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className={styles.root}>
       <Container>
         <h1>Explore Space-X launches!</h1>
       </Container>
