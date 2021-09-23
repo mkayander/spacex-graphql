156f4a0d98976fc40c74b634f6d8678f4fb10d56
Max Kayander <maxim.kayander1@gmail.com>
2021-09-23 16:52:24 +0300

    Control if data collection is full.

--- src/store/slices/launchesSlice.ts
+++ src/store/slices/launchesSlice.ts
@@ -10,12 +10,14 @@ export type Launch = ArrayElement<GetLaunchesQuery["launchesPast"]>;
 type LaunchesState = {
   loading: boolean;
   offsetStep: number;
+  isFull: boolean;
   data: Launch[];
   error?: Error;
 };
 
 const initialState: LaunchesState = {
   loading: false,
+  isFull: false,
   data: [],
   offsetStep: 10,
 };
@@ -27,8 +29,9 @@ const launchesSlice = createSlice({
     fetch: state => {
       state.loading = true;
     },
-    finish: state => {
+    finish: (state, action: PayloadAction<boolean>) => {
       state.loading = false;
+      state.isFull = action.payload;
     },
     updateAll: (state, action: PayloadAction<Launch[]>) => {
       state.data = action.payload;
@@ -54,6 +57,8 @@ export const fetchNextLaunches = (): ThunkAction<void, RootState, unknown, any> 
   return async (dispatch, getState) => {
     dispatch(launchesActions.fetch());
 
+    let isFull = false;
+
     try {
       const { offsetStep, data } = getState().launches;
       const response = await apollo.query<GetLaunchesQuery, GetLaunchesQueryVariables>({
@@ -62,15 +67,18 @@ export const fetchNextLaunches = (): ThunkAction<void, RootState, unknown, any> 
       });
       const result = response.data.launchesPast;
 
+      console.log("Got result - ", result);
+
       if (result?.length && result.length > 0) {
         dispatch(launchesActions.append(result));
       } else {
-        dispatch(launchesActions.error(new Error("Got empty launches result!")));
+        isFull = true;
+        // dispatch(launchesActions.error(new Error("Got empty launches result!")));
       }
     } catch (e) {
       dispatch(launchesActions.error(e as Error));
     } finally {
-      dispatch(launchesActions.finish());
+      dispatch(launchesActions.finish(isFull));
     }
   };
 };
